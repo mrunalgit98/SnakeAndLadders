ac9c6df3871a34653f08e0bf9ae6aaa3685fe7bc
mrunal <mrunalbhoir100@gmail.com>
2022-07-12 12:35:29 +0530

    UC3

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,11 +1,38 @@
-//The Player rolls the die to get a number between 1 to 6.
+//Player then checks for a Option. They are No Play, Ladder or Snake. - Use ((RANDOM)) to check for Options
+
+import java.util.Scanner;
 
 public class SnakeAndLadder{
-	public static void main(String[] args) {
-		int startposition=0;
-		int dieRoll=(int)(Math.floor(Math.random() * 10)%6 + 1);
-		System.out.println("start position--" +startposition);
-		System.out.println("die rolled-- " +dieRoll);
-				
-	}
+    public static void main(String[] args) {
+        int startPosition = 0;
+        System.out.println("Enter your name-- ");
+        Scanner sc = new Scanner(System.in);
+        String Player_Name = sc.next();
+
+        int dieRoll = (int) (Math.floor(Math.random() * 10) % 6);
+        System.out.println("Dice Roll number --" + dieRoll);
+
+        int check = (int) (Math.floor(Math.random() * 10) % 3);
+
+        switch (check)
+
+        {
+            case 0:
+                System.out.println("Snake");
+                startPosition = dieRoll;
+                break;
+
+            case 1:
+                System.out.println("Ladder");
+                startPosition = dieRoll;
+                break;
+
+            default:
+                System.out.println("NO PLAY");
+                startPosition = 0;
+        }
+        System.out.println(" Player " + Player_Name + " On Position :" + startPosition);
+
+
+    }
 }
