2e2496b135593bb4139792f5b48de8dd0a9d52d8
mrunal <mrunalbhoir100@gmail.com>
2022-07-12 12:38:31 +0530

    UC5

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,47 +1,40 @@
-//Repeat till the Player reaches the winning position 100.
-
+//Ensure the player gets to exact winning position 100.
 
 import java.util.Scanner;
 
-public class SnakeAndLadder {
-
-    public static final int WINNING_POSITION = 100;
-
-    public static void main(String[] args) {
-        int startPosition = 0;
-        System.out.println("Enter your name: ");
-        Scanner sc = new Scanner(System.in);
-        String Player_Name = sc.next();
-
-        while (WINNING_POSITION > startPosition) {
-
-            int dieRoll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-            System.out.println("Dice Roll number :" + dieRoll);
-
-            int check = (int) (Math.floor(Math.random() * 10) % 3);
-
-            switch (check) {
-                case 0:
-                    System.out.println("No Play");
-                    startPosition += 0;
-                    break;
-
-                case 1:
-                    System.out.println("Ladder:" + " + " + dieRoll);
-                    startPosition += dieRoll;
-                    break;
-
-                default:
-                    System.out.println("Snake : " + " - " + dieRoll);
-                    startPosition += dieRoll;
-
-
-            }
-
-        }
-
-        System.out.println("Player " + Player_Name + " On Position :" + startPosition);
-
-
-    }
+public class SnakeAndLadder{
+	public static final int Winning_Position=100;
+	public static void main(String[] args) {
+		int startPosition=0;
+		System.out.println("Enter Name--");
+		Scanner sc = new Scanner(System.in);
+		String Player_Name = sc.next();
+		while(Winning_Position>startPosition) {
+			int dieRoll = (int)(Math.floor(Math.random()*10)%6 + 1);
+			int toWin = Winning_Position - startPosition;
+			int checkOption=(int)(Math.floor(Math.random()*10)%3 );
+			
+		 if(toWin>=dieRoll) {
+			 
+			 switch (checkOption) {
+			 case 0:
+				 System.out.println(" snake " +dieRoll);
+				 startPosition -=0;
+				 break;
+				 
+			 case 1:
+				 System.out.println("ladder  " + dieRoll);
+				 startPosition +=dieRoll;
+				 break;
+				 
+				 default:
+					 System.out.println("Start again");
+					 startPosition += dieRoll;
+					 
+					
+			   }
+			 }
+		   }
+		System.out.println("Player" + Player_Name + "winning position" +startPosition);
+		}
 }
