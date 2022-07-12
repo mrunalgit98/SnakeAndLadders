269182ecfe66825475d86a2d46a65c2b66fda222
mrunal <mrunalbhoir100@gmail.com>
2022-07-12 12:33:51 +0530

    UC2

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,10 +1,11 @@
-//Snake and Ladder game played with single player at start position 0
+//The Player rolls the die to get a number between 1 to 6.
 
-
-public class SnakeAndLadder {
-public static void main(String[] args) {
-	int player=0;
-	System.out.println("Start position=" +player);
-	
-}
+public class SnakeAndLadder{
+	public static void main(String[] args) {
+		int startposition=0;
+		int dieRoll=(int)(Math.floor(Math.random() * 10)%6 + 1);
+		System.out.println("start position--" +startposition);
+		System.out.println("die rolled-- " +dieRoll);
+				
+	}
 }
