6c7c15576b3ff5765b26b3f3b39917c563447a59
mrunal <mrunalbhoir100@gmail.com>
2022-07-12 12:39:39 +0530

    UC6

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,40 +1,49 @@
-//Ensure the player gets to exact winning position 100.
+//Report the number of times the dice was played to win the game and also the position after every die role
 
 import java.util.Scanner;
 
 public class SnakeAndLadder{
+	
 	public static final int Winning_Position=100;
+	
 	public static void main(String[] args) {
-		int startPosition=0;
-		System.out.println("Enter Name--");
+		int startPosition = 0;
+		int rollCounter = 0;
+		System.out.println("Enter Name");
 		Scanner sc = new Scanner(System.in);
 		String Player_Name = sc.next();
-		while(Winning_Position>startPosition) {
-			int dieRoll = (int)(Math.floor(Math.random()*10)%6 + 1);
-			int toWin = Winning_Position - startPosition;
-			int checkOption=(int)(Math.floor(Math.random()*10)%3 );
-			
-		 if(toWin>=dieRoll) {
-			 
-			 switch (checkOption) {
-			 case 0:
-				 System.out.println(" snake " +dieRoll);
-				 startPosition -=0;
-				 break;
-				 
-			 case 1:
-				 System.out.println("ladder  " + dieRoll);
-				 startPosition +=dieRoll;
-				 break;
-				 
-				 default:
-					 System.out.println("Start again");
-					 startPosition += dieRoll;
-					 
-					
-			   }
-			 }
-		   }
-		System.out.println("Player" + Player_Name + "winning position" +startPosition);
-		}
+		
+		while (Winning_Position > startPosition) {
+			int dieRoll = (int) (Math.floor(Math.random()*10)%6);
+			rollCounter++;
+			int toWin = Winning_Position -startPosition;
+            int check=(int)(Math.floor(Math.random()*10)%3);
+            
+            if (toWin >= dieRoll) {
+            	
+            	switch (check) {
+            	
+            	case 0:
+            		startPosition+=0;
+            		break;
+            		
+            	case 1:
+            		startPosition += dieRoll;
+            		break;
+            		
+            		default:
+            			startPosition -= dieRoll;
+            			
+            			if(startPosition <0) {
+            				System.out.println("start again");
+            				startPosition=0;
+            			}
+            		
+            	}
+            }
+            System.out.println("Player Position " +startPosition);
+	}
+	 System.out.println("player " + Player_Name + " Winning Position " +startPosition );
+	 System.out.println("dice rolled " +rollCounter);
+}
 }
