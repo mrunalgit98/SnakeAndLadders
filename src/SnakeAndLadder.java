783b8b1c7ebe5e32975e3ec7bea9418e7a71aabe
mrunal <mrunalbhoir100@gmail.com>
2022-07-12 12:31:40 +0530

    UC1

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,4 +1,10 @@
+//Snake and Ladder game played with single player at start position 0
+
 
 public class SnakeAndLadder {
-
+public static void main(String[] args) {
+	int player=0;
+	System.out.println("Start position=" +player);
+	
+}
 }
